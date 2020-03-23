c09c185f7f0ef8e73c124bc88faebc08a0f562ab
StasyHard <reyngardt.as@yandex.ru>
2020-03-23 12:31:04 +0300

    Init

---
+++ Set/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Set
+//
+//  Created by Anastasia Reyngardt on 23.03.2020.
+//  Copyright © 2020 GermanyHome. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
